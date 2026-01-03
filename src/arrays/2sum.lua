a95ce02ecd857d41ed79ce394255d7caaca75e14
Miled Mahdoui <m.mahdoui@hotmail.de>
2026-01-03 09:09:28 +0100

    bessere lösung auch gefunden

--- src/arrays/2sum.lua
+++ src/arrays/2sum.lua
@@ -1,46 +1,57 @@
-local target = 9
-local nums = { 2, 7, 11, 15 }
-local nums2 = {3,2,4}
-local target2 = 6
-
 --- Finds two indices i, j such that nums[i] + nums[j] == target (brute force).
-local function two_sum_bruteforce(nums, target, debug)
+local function two_sum_bruteforce(nums, target)
   for i = 1, #nums do
     for j = i + 1, #nums do
       local sum = nums[i] + nums[j]
-
-      if debug then
-        print(("i=%d j=%d sum=%d"):format(i, j, sum))
-      end
-
       if sum == target then
-        return i, j
+        return nums[i], nums[j]
       end
     end
   end
   return nil, nil
 end
 
+-- Better algorithm, uses O(n) time and 0(n^2) space complexity, space complexity is worse
+
+local function two_sum_maping(nums, target)
+  local seen = {}
+  for i = 1, #nums do
+    local complement = target - nums[i]
+
+    if seen[complement] ~= nil
+    then
+      return complement, nums[i]
+    else
+      seen[nums[i]] = i
+    end
+  end
+end
+
 
 --Test cases
 local test_cases = {
-  { nums = {2, 7, 11, 15}, target = 9 },
-  { nums = {3, 2, 4}, target = 6 },
-  { nums = {1, 2, 3}, target = 10 },
+  { nums = { 2, 7, 11, 15 }, target = 9 },
+  { nums = { 3, 2, 4 },      target = 6 },
+  { nums = { 1, 2, 3 },      target = 10 },
 }
 
 
 
 --Test case execution
 for _, test in ipairs(test_cases) do
-  local i, j = two_sum_bruteforce(test.nums, test.target, false)
+  local i1, j1 = two_sum_maping(test.nums, test.target)
+  local i2, j2 = two_sum_bruteforce(test.nums, test.target)
 
-  if i and j then
-    print(("Target=%d → i=%d j=%d (%d + %d)")
-      :format(test.target, i, j, test.nums[i], test.nums[j]))
+  print("Mapping:")
+  if i1 and j1 then
+    print(i1 .. " and " .. j1)
   else
-    print(("Target=%d → No pair found"):format(test.target))
+    print("No pair found")
+  end
+  print("Brute force")
+  if i2 and j2 then
+    print(i2 .. " and " .. j2)
+  else
+    print("No pair found")
   end
 end
-
-
